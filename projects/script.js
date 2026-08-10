4b20e85c78273004fc26a85ed8c0266523b6db8f
Aftab Sipahi <sipahiaftab@gmail.com>
2026-08-10 14:26:51 +0530

    Project and Educa images update

--- projects/script.js
+++ projects/script.js
@@ -45,23 +45,20 @@ function showProjects(projects) {
     let projectsHTML = "";
     projects.forEach(project => {
         projectsHTML += `
-        <div class="grid-item ${project.category}">
-        <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+        <div class="box tilt">
+      <div class="project-visual">
+        <span>${project.industry}</span>
+      </div>
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
         </div>
         <div class="desc">
           <p>${project.desc}</p>
-          <div class="btns">
-            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-          </div>
         </div>
       </div>
     </div>
-    </div>`
+    `
     });
     projectsContainer.innerHTML = projectsHTML;
 
@@ -82,22 +79,6 @@ function showProjects(projects) {
     // /* SCROLL PROJECTS */
     // srtop.reveal('.work .box', { interval: 200 });
 
-    // isotope filter products
-    var $grid = $('.box-container').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows',
-        masonry: {
-            columnWidth: 200
-        }
-    });
-
-    // filter items on button click
-    $('.button-group').on('click', 'button', function () {
-        $('.button-group').find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
 }
 
 getProjects().then(data => {
