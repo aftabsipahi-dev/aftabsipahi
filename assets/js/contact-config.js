c6233109a1bd747efb9297b8c8751f63bf038c5e
Aftab Sipahi <sipahiaftab@gmail.com>
2026-08-12 11:34:32 +0530

    new deployed

--- assets/js/contact-config.js
+++ assets/js/contact-config.js
@@ -1,5 +1,5 @@
 window.PORTFOLIO_CONTACT_CONFIG = {
-    appsScriptEndpoint: "https://script.google.com/macros/s/AKfycbwIKSU6IbslveC6I2BWRWWF1OFVmviFYtLCGqnHcw6eDZ78Eb4WM8kbfL4ScVA21Z0A/exec",
+    appsScriptEndpoint: "https://script.google.com/macros/s/AKfycbyrixoF942mi2n398GJRJlLF2P1X-kqfi0UsqvmbzY4u3-Po7WjDQkmstZGJcvlXIJT/exec",
     notificationEmail: "aftab@aftabsipahi.com",
     defaultOpportunityStatus: "Open",
     sourceLabel: "Portfolio Website"
